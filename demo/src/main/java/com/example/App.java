27d0e4c78c12a0ffed97b5465f5e16a88d044957
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 10:12:13 +0200

    finito

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // System.out.println( "Hello World!" );
+        try {
+            System.out.print("creo la socket\n");
+            Socket s = new Socket("10.22.9.14", 3000);
+
+            BufferedReader inTastiera = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader inServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            //String string = inTastiera.readLine();
+            System.out.print("preso da tastiera la stringa\n");
+
+            out.writeBytes(inTastiera.readLine() + "\n");
+            // System.out.print("inviato al server\n");
+
+            System.out.print(inServer.readLine() + "\n");
+
+            s.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
